7ab57a6170efa3ba550dc953d5646b6ebf528d01
Nidula Mallikarachchi <nidulamallikarachchi@gmail.com>
2024-12-06 16:48:54 +1100

    Navbar Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
-import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
-import Services from "./pages/Services.jsx";
-import Contact from "./pages/Contact.jsx";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
 
-export default function App() {
+const App = () => {
     return (
-        <>
-            <Home/>
-            <About/>
-            <Services/>
-            <Contact/>
-            <Footer/>
-        </>
+        <Router>
+            <Navbar />
+            <main className="min-h-screen">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/services" element={<Services />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </main>
+            <Footer />
+        </Router>
+    );
+};
 
-    )
-}
+export default App;
